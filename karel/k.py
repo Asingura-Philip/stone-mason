1738b8e9b34dd111e986902b5f43c6e525fef0b3
Asingura-Philip <134685900+Asingura-Philip@users.noreply.github.com>
2023-05-30 15:28:29 +0300

    Add files via upload

---
+++ karel/k.py
@@ -0,0 +1,55 @@
+from stanfordkarel import *
+
+""""
+def main():
+    turn_left()
+    pick_beeper()
+    for i in range(4):
+        move_pick()
+
+
+def move_pick():
+    move()
+    pick_beeper()
+"""
+
+
+def main():
+    pick_all()
+    steps()
+    turn_side()
+    pick_all()
+
+
+def half():
+    turn_left()
+    for i in range(4):
+        pick_move()
+    pick_beeper()
+    turn_left()
+
+
+def pick_move():
+    pick_beeper()
+    move()
+
+
+def steps():
+    for i in range(4):
+        move()
+
+
+def turn_side():
+    for i in range(2):
+        turn_left()
+
+
+def pick_all():
+    half()
+    steps()
+    half()
+    turn_side()
+
+
+if __name__ == "__main__":
+    run_karel_program()
